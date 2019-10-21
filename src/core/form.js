ef6976f66e41e8fc7e2001978618781ba2229e9d
Raite17 <survivarian@gmail.com>
2019-10-21 22:11:25 +0600

    form component and validation was created

--- src/core/form.js
+++ src/core/form.js
@@ -11,4 +11,42 @@ export class Form {
         }
         return value;
     }
+
+    clear() {
+        for (let control in this.controls) {
+            this.form[control].value = '';
+        }
+    }
+
+    isValid() {
+        let isFormValid = true;
+        for (let control in this.controls) {
+            const validators = this.controls[control];
+            let isValid = true;
+
+            for (let validator of validators) {
+                isValid = validator(this.form[control].value) && isValid;
+            }
+
+            isValid ? clearError(this.form[control]) : setError(this.form[control]);
+
+            isFormValid = isFormValid && isValid;
+        }
+        return isFormValid;
+    }
+}
+
+function setError($control) {
+    clearError($control);
+    const error = '<p class="validation-error">Введите корректное значение</p>';
+    $control.classList.add('invalid');
+    $control.insertAdjacentHTML('afterend', error);
+}
+
+function clearError($control) {
+    $control.classList.remove('invalid');
+    if ($control.nextSibling) {
+        $control.closest('.form-control').removeChild($control.nextSibling);
+    }
+
 }

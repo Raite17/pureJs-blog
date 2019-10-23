67e43b31f12025e74883acdc7571ee8399eeff41
Raite17 <survivarian@gmail.com>
2019-10-23 22:38:05 +0600

    fetch-posts and render posts functions was created

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,7 +1,42 @@
-import { Component } from '../core/component';
+import { Component } from "../core/component";
+import { apiService } from "../services/api.service";
+import { TransformService } from "../services/transform.service";
 
 export class PostComponent extends Component {
     constructor(id) {
         super(id);
     }
+
+    async onShow() {
+        const fbData = await apiService.fetchPosts();
+        const post = TransformService.fbObjectToArray(fbData);
+        const html = post.map(post => renderPost(post));
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '));
+    }
+
+    onHide() {
+        this.$el.innerHTML = '';
+    }
+}
+
+function renderPost(post) {
+    const tag = post.type === 'news' ?
+        '<li class="tag tag-blue tag-rounded">Новость</li>' :
+        '<li class="tag tag-rounded">Заметка</li>'
+    const button = '<button class="button-round button-small button-primary">Сохранить</button>';
+    return `
+    <div class="panel">
+        <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags">
+                ${tag}
+            </ul>
+        </div>
+        <div class="panel-body">
+            <p class="multi-line">${post.fulltext}</p>
+        </div>
+        <div class="panel-footer w-panel-footer">
+             ${button}
+        </div>
+    </div>`;
 }
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -9,12 +9,27 @@ class ApiService {
                 method: 'post',
                 body: JSON.stringify(post)
             });
-            const repsonse = await fetch(request);
-            return await repsonse.json();
+            return useRequest(request);
         } catch (e) {
             console.error(e);
         }
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            });
+            return useRequest(request);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+async function useRequest(request) {
+    const repsonse = await fetch(request);
+    return await repsonse.json();
 }
 
 export const apiService = new ApiService('https://nativejs-blog-991eb.firebaseio.com');
